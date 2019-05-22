af2cef7e1ae9cd9eb915e5dd2c523cc135682f77
dev <dev@sint.kr>
2019-05-22 11:48:19 +0900

    modfiy: directory change

--- src/spa/View.vue
+++ src/spa/View.vue
@@ -1,11 +1,84 @@
 <!-- READ ONLY -->
 <template>
-<div class="container">
-
-</div>
+  <div class="container">
+    <div class="row editor">
+      <editor-content class="col-12 mt4 editor__content" :editor="editor" />
+    </div>
+  </div>
 </template>
 
 <script>
-import data from '@/data'
-export default {}
+import { Editor, EditorContent, EditorMenuBubble } from 'tiptap'
+import {
+  Blockquote,
+  BulletList,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  ListItem,
+  OrderedList,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  History,
+} from 'tiptap-extensions'
+export default {
+  components: {
+    EditorContent,
+    EditorMenuBubble,
+  },
+  data() {
+    return {
+      editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new History(),
+        ],
+        content: `
+          <h2>
+            Links
+          </h2>
+          <p>
+            Try to add some links to the <a href="https://en.wikipedia.org/wiki/World_Wide_Web">world wide web</a>. By default every link will get a <code>rel="noopener noreferrer nofollow"</code> attribute.
+          </p>
+        `,
+      }),
+      linkUrl: null,
+      linkMenuIsActive: false,
+    }
+  },
+  methods: {
+    showLinkMenu(attrs) {
+      this.linkUrl = attrs.href
+      this.linkMenuIsActive = true
+      this.$nextTick(() => {
+        this.$refs.linkInput.focus()
+      })
+    },
+    hideLinkMenu() {
+      this.linkUrl = null
+      this.linkMenuIsActive = false
+    },
+    setLinkUrl(command, url) {
+      command({ href: url })
+      this.hideLinkMenu()
+      this.editor.focus()
+    },
+  },
+}
 </script>
